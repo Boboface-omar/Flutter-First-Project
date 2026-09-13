4655ba48808caf44e49121c79c613d68b5a34cc4
Boboface <oumarbobo.diallo1@unchk.edu.sn>
2026-09-13 15:22:02 +0000

    StateFulWidget: Revision

--- flutter_lab/lib/main.dart
+++ flutter_lab/lib/main.dart
@@ -1,72 +1,45 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Column(
-            children: [
-              CarteVisite(nom: 'Charlie', metier: 'Développeur', ville: 'Paris'),
-              const SizedBox(height: 20),
-              CarteVisite(nom: 'Bob Martin', metier: 'Designer', ville: 'Lyon'),
-            ],
-          ),
-        ),
-      ),
-    ),
-  );
+  runApp(const MaterialApp(home: Scaffold(body: MonCompteur())));
 }
 
-class CarteVisite extends StatefulWidget {
-  final String nom;
-  final String metier;
-  final String ville;
-
-  const CarteVisite({
-    super.key,
-    required this.nom,
-    required this.metier,
-    required this.ville,
-  });
+class MonCompteur extends StatefulWidget {
+  const MonCompteur({super.key});
 
   @override
-  State<CarteVisite> createState() => _CarteVisiteState();
+  State<MonCompteur> createState() => _MonCompteurState();
 }
 
-class _CarteVisiteState extends State<CarteVisite> {
-  int likes = 0;
-
+class _MonCompteurState extends State<MonCompteur> {
+  int clics = 0;
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(widget.nom),
-                const SizedBox(width: 10),
-                Text(widget.metier),
-                const SizedBox(width: 10),
-                Text(widget.ville),
-              ],
-            ),
-            const SizedBox(height: 10),
-            Text('❤️ $likes j\'aime'),
-            const SizedBox(height: 10),
-            ElevatedButton(
-              onPressed: () {
-                setState(() {
-                  likes++;
-                });
-              },
-              child: const Text('J\'aime'),
-            ),
-          ],
-        ),
+    // TODO: implement build
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text('Clics : $clics', style: const TextStyle(fontSize: 30)),
+          const SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: () {
+              setState(() {
+                clics++;
+              });
+            },
+            child: const Text('Cliquer ici'),
+          ),
+          const SizedBox(height: 10),
+          ElevatedButton(
+            onPressed: () {
+              setState(() {
+                clics = 0;
+              });
+            },
+            child: const Text('Reset'),
+          ),
+        ],
       ),
     );
   }
