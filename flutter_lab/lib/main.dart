9b51e0aaf48b72159d03b4089925e80c41bd705f
Boboface <oumarbobo.diallo1@unchk.edu.sn>
2026-09-17 15:30:24 +0000

    Flutter Layout Suite

--- flutter_lab/lib/main.dart
+++ flutter_lab/lib/main.dart
@@ -4,15 +4,22 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Container(
-            width: 200,
-            height: 100,
-            color: Colors.blue,
-            child: const Center(child: Text('Bonjour Flutter'),),
-          ),
-        ),
+        body: Center(child: Padding(padding: EdgeInsets.all(20),
+        child : Row(children: [Carte(nom: 'Diallo', metier: 'Developpeur'),
+        SizedBox(width: 20,), Carte(nom: 'Pierre', metier: 'Electricien')]))),
       ),
     ),
   );
+}
+
+class Carte extends StatelessWidget {
+  final String nom;
+  final String metier;
+  const Carte({super.key, required this.nom, required this.metier});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Card(child: Column(children: [Text(nom), Text(metier)]));
+  }
 }
