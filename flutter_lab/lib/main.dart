75757ba632fd9e7415acee8917149d100d317b9f
Boboface <oumarbobo.diallo1@unchk.edu.sn>
2026-09-18 18:38:34 +0000

    Flutter Layout Lea axes: Revision

--- flutter_lab/lib/main.dart
+++ flutter_lab/lib/main.dart
@@ -1,17 +1,46 @@
 import 'package:flutter/material.dart';
 
+// void main() {
+//   runApp(
+//     const MaterialApp(
+//       home: Scaffold(
+//         body: Center(
+//           child: Column(
+//             mainAxisAlignment: MainAxisAlignment.center,
+//             crossAxisAlignment: CrossAxisAlignment.start,
+//             children: [
+//               Text('Flutter'),
+//               SizedBox(height: 15),
+//               Text('Développement'),
+//               SizedBox(height: 20),
+//               Text('Mobile & Web'),
+//             ],
+//           ),
+//         ),
+//       ),
+//     ),
+//   );
+// }
+import 'package:flutter/material.dart';
+
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      body: Center(
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Icon(Icons.person),
-            Text('Oumar')
-          ],
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text('[ + ]'),
+              SizedBox(width: 15),
+              Text('[ Reset ]'),
+              SizedBox(width: 15),
+              Text('[ - ]'),
+            ],
+          ),
         ),
       ),
     ),
-  ));
+  );
 }
