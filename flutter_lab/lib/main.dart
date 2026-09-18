b66e9b5c910938e8b7be9457d6ac2106942f7318
Boboface <oumarbobo.diallo1@unchk.edu.sn>
2026-09-18 18:16:51 +0000

    Flutter Layout Lea axes

--- flutter_lab/lib/main.dart
+++ flutter_lab/lib/main.dart
@@ -1,25 +1,17 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        body: Center(child: Padding(padding: EdgeInsets.all(20),
-        child : Row(children: [Carte(nom: 'Diallo', metier: 'Developpeur'),
-        SizedBox(width: 20,), Carte(nom: 'Pierre', metier: 'Electricien')]))),
+  runApp(MaterialApp(
+    home: Scaffold(
+      body: Center(
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Icon(Icons.person),
+            Text('Oumar')
+          ],
+        ),
       ),
     ),
-  );
-}
-
-class Carte extends StatelessWidget {
-  final String nom;
-  final String metier;
-  const Carte({super.key, required this.nom, required this.metier});
-
-  @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    return Card(child: Column(children: [Text(nom), Text(metier)]));
-  }
+  ));
 }
