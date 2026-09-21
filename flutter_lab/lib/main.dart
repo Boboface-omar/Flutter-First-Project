67380aff9062d0dbdf9536e7d3361305fe97ce0f
Boboface <oumarbobo.diallo1@unchk.edu.sn>
2026-09-21 12:58:39 +0000

    Flutter Layout Expanded

--- flutter_lab/lib/main.dart
+++ flutter_lab/lib/main.dart
@@ -1,42 +1,15 @@
 import 'package:flutter/material.dart';
 
-// void main() {
-//   runApp(
-//     const MaterialApp(
-//       home: Scaffold(
-//         body: Center(
-//           child: Column(
-//             mainAxisAlignment: MainAxisAlignment.center,
-//             crossAxisAlignment: CrossAxisAlignment.start,
-//             children: [
-//               Text('Flutter'),
-//               SizedBox(height: 15),
-//               Text('Développement'),
-//               SizedBox(height: 20),
-//               Text('Mobile & Web'),
-//             ],
-//           ),
-//         ),
-//       ),
-//     ),
-//   );
-// }
-import 'package:flutter/material.dart';
-
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
+          child: Column(
             children: [
-              Text('[ + ]'),
-              SizedBox(width: 15),
-              Text('[ Reset ]'),
-              SizedBox(width: 15),
-              Text('[ - ]'),
+              Expanded(child: Container(color: Colors.yellow)),
+              Expanded(child: Container(color: Colors.blue)),
+              Expanded(child: Container(color: Colors.red))
             ],
           ),
         ),
