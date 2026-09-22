323c73cf8a3ac0950ce461fe04cb01d6891fcc0e
Boboface <oumarbobo.diallo1@unchk.edu.sn>
2026-09-22 14:39:04 +0000

    Flutter Layout STACK

--- flutter_lab/lib/main.dart
+++ flutter_lab/lib/main.dart
@@ -1,15 +1,36 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp(home: Scaffold(body: ResponsiveExample())));
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: MonStack(),
+        ),
+      ),
+    ),
+  );
 }
 
-class ResponsiveExample extends StatelessWidget {
-  const ResponsiveExample({super.key});
+class MonStack extends StatelessWidget {
+  const MonStack({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final height = MediaQuery.of(context).size.height.toStringAsFixed(0);
-    return Center(child: Text('Largeur : $height'));
+    return Stack(
+      children: [
+        // 1. Carré rouge 200x200 (derrière)
+        Container(width: 200, height: 200, color: Colors.red),
+
+        // 2. Carré vert 150x150 (au-dessus)
+        Container(width: 150, height: 150, color: Colors.green),
+
+        // 3. Carré bleu 100x100 (au-dessus)
+        Container(width: 100, height: 100, color: Colors.blue),
+
+        // 4. Texte tout devant
+        const Text('DEVANT', style: TextStyle(color: Colors.white, fontSize: 20)),
+      ],
+    );
   }
 }
